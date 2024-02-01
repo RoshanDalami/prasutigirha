c2f39b1e6a017469d39ff660e1f22f17843bae1b
RoshanDalami <roshandalami0@gmail.com>
2024-02-01 13:51:19 +0545

    added login feature

--- src/app/api/user/login/route.js
+++ src/app/api/user/login/route.js
@@ -8,6 +8,7 @@ connect()
 
 export async function POST(req,res){
     const JWT_SECRET = "sjanxdskc";
+    let userDetail ;
     try {
         const {email,password} = await req.json();
         const user = await User.findOne({email:email});
@@ -15,17 +16,27 @@ export async function POST(req,res){
             return NextResponse.json({message:"Email or password is Invalid"},{status:201})
         }
 
-        const matchedPassword = await bcryptjs.compare(password,user.password);
+        const matchedPassword = await bcryptjs.compare(password,user.password)
+       
         if(!matchedPassword){
             return NextResponse.json({message:"Password Invalid"},{status:201})
+        }
+        if(matchedPassword){
+             userDetail = await User.findOne({email:email},{__v:0}).select("-password -confirmPassword -createdAt -updatedAt")
         }
         const token = jwt.sign(
             {userId:user._id,email:user.email},
             JWT_SECRET,{expiresIn:'1d'}
         )
-        return NextResponse.json(token,{message:"User Logged In Successfully"},{status:200})
+        
+        const response =  NextResponse.json(userDetail,{message:"User Logged In Successfully"},{status:200})
+        response.cookies.set('token',token,{
+            httpOnly:true,
+        })
+        return response
         
     } catch (error) {
+        console.log(error)
         return NextResponse.json({message:"Internal Server Error"},{status:500})
     }
 }
