77ac9cbd10092001dc71e01f604ca5ae089d1105
sandeepsz <sandeep47nepali@gmail.com>
2024-02-01 15:50:24 +0545

    create api for daanDarta

--- src/Model/donorDetails.model.js
+++ src/Model/donorDetails.model.js
@@ -1,4 +1,68 @@
-import mongoose , {Schema} from 'mongoose'
+import mongoose, { Schema } from "mongoose";
+import { Gestational } from "../Model/dropdownModels/gestational.model.js";
+import { BabyStatus } from "../Model/dropdownModels/babyStatus.model.js";
+import { Parity } from "../Model/dropdownModels/parity.model.js";
+import { Delivery } from "../Model/dropdownModels/delivery.model.js";
 
+const daanDartaSchema = new mongoose.Schema({
+  hosRegNo: {
+    type: String,
+  },
+  donorRegNo: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  date: {
+    type: Date,
+  },
+  time: {
+    type: String,
+  },
+  donor_FullName: {
+    type: String,
+  },
+  education: {
+    type: String,
+  },
+  ethnicity: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  contactNo: {
+    type: String,
+  },
+  ageOfChild: {
+    type: Number,
+  },
+  gestationalAge: {
+    type: Schema.Types.ObjectId,
+    ref: Gestational,
+  },
+  modeOfDelivery: {
+    type: Schema.Types.ObjectId,
+    ref: Delivery,
+  },
+  parity: {
+    type: Schema.Types.ObjectId,
+    ref: Parity,
+  },
 
-export const DaanDarta = mongoose.models.DaanDarta || mongoose.model("DaanDarta", daanDartaSchema);
+  babyStatus: {
+    type: Schema.Types.ObjectId,
+    ref: BabyStatus,
+  },
+
+  // serologyRecords: serologyScreeningSchema,
+
+  // verbalExamination: {
+  //   type: verbalExaminationSchema,
+  // },
+
+  // donorPhysicalExamination: donorPhysicalExaminationSchema,
+});
+
+export const DaanDarta =
+  mongoose.models.DaanDarta || mongoose.model("DaanDarta", daanDartaSchema);
