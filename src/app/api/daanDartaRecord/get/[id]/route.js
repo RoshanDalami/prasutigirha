2e24e40372e2cab91d4a6999f0f8a71126f72700
RoshanDalami <roshandalami0@gmail.com>
2024-02-04 16:25:53 +0545

    same for

--- src/app/api/daanDartaRecord/get/[id]/route.js
+++ src/app/api/daanDartaRecord/get/[id]/route.js
@@ -2,12 +2,13 @@ import { DaanDarta } from "src/Model/donorDetails.model";
 import {NextResponse} from 'next/server';
 
 export async function GET(req,{params}){
+    const id = params.id;
     try {
-        const id = params.id;
         const response = await DaanDarta.findOne({_id:id},{__v:0});
         return NextResponse.json(response,{status:200})
         
     } catch (error) {
+        console.log(error)
         return NextResponse.json({message:"Internal Server Error"},{status:500})
     }
 }
