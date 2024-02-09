59ee48af30bb6fe89ae4a125decf938bcd70ab15
sandip <sandeep47nepali@gmail.com>
2024-02-09 15:49:26 +0545

    updatd

--- src/app/api/pooling/colostrum/route.js
+++ src/app/api/pooling/colostrum/route.js
@@ -4,15 +4,27 @@ import { MilkVolume } from "src/Model/volumeOfMilk.model";
 export async function GET() {
   try {
     const DonarData = await DaanDarta.find();
-    const filteredDonarData = DonarData.filter((donar) => {
-      if (donar.babyStatus && donar.babyStatus.dateOfBirth) {
+
+    const filteredDonarData = DonarData.filter(async (donar) => {
+      if (donar.babyStatus && donar.babyStatus.engDateBirth) {
         const currentDate = new Date();
-        const dob = new Date(donar.babyStatus.dateOfBirth);
+        const dob = new Date(donar.babyStatus.engDateBirth);
         const diffTime = Math.abs(currentDate - dob);
         const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
         donar.updatedAgeOFChild = diffDays;
-        console.log(donar.updatedAgeOFChild, "<======");
-        return diffDays <= 3;
+        donar.save();
+
+        if (diffDays <= 3) {
+          const donarId = donar._id;
+          const milkVolume = await MilkVolume.find();
+          const filteredMilkVolume = milkVolume.filter((milk) => {
+            if (milk.donorId == donarId) {
+              return milk.donorId == donarId;
+            }
+          });
+          console.log("filteredMilkVolume", filteredMilkVolume);
+          return filteredMilkVolume;
+        }
       }
       return false;
     });
