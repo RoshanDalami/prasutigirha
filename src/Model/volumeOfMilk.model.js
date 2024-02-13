014f425d38bdb68380733a9b7bb5842d9149c363
sandip <sandeep47nepali@gmail.com>
2024-02-13 13:11:43 +0545

    added seacrh functionality

--- src/Model/volumeOfMilk.model.js
+++ src/Model/volumeOfMilk.model.js
@@ -1,52 +1,55 @@
 import mongoose, { Schema } from "mongoose";
 
-
-const volumeOfMilkSchema = new Schema({
-    userId:{
-        type:Schema.Types.ObjectId,
-        ref:'User'
+const volumeOfMilkSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-    donorId:{
-        type:Schema.Types.ObjectId,
-        ref:'DaanDarta'
+    donorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "DaanDarta",
     },
-    donorName:{
-        type:String,
-        required:true
+    donorName: {
+      type: String,
+      required: true,
     },
-    gestationalAge:{
-        type: Number ,
-        required:true
+    gestationalAge: {
+      type: Number,
+      required: true,
     },
-    date:{
-        type:String,
-        required:true
+    date: {
+      type: String,
+      required: true,
     },
-    engDate:{
-        type:String,
-        required:true
+    engDate: {
+      type: String,
+      required: true,
     },
-    time:{
-        type:String,
-        required:true
+    time: {
+      type: String,
+      required: true,
     },
-    quantity:{
-        type:Number,
-        required:true
+    quantity: {
+      type: Number,
+      required: true,
     },
-    storedBy:{
-        type:String,
-        required:true
+    storedBy: {
+      type: String,
+      required: true,
     },
-    temp:{
-        type:Number,
-        required:true
+    temp: {
+      type: Number,
+      required: true,
     },
-    remaining:{
-        type:Number,
-        required:true
-    }
-},{timestamps:true})
-const MilkVolume = mongoose.models.MilkVolume || mongoose.model('MilkVolume',volumeOfMilkSchema)
-export {MilkVolume}
-
+    remaining: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+const MilkVolume =
+  mongoose.models.MilkVolume ||
+  mongoose.model("MilkVolume", volumeOfMilkSchema);
+export { MilkVolume };
--- src/app/api/pooling/condition/[id]/route.js
+++ src/app/api/pooling/condition/[id]/route.js
@@ -5,7 +5,9 @@ import { DaanDarta } from "src/Model/donorDetails.model";
 export async function GET(req, { params }) {
   const id = params.id;
   try {
-    const response = await MilkVolume.find({gestationalAge:id});
+    const response = await MilkVolume.find(
+      { gestationalAge: id } && { remaining: { $gt: 0 } }
+    );
     const extractedData = response.map((item) => ({
       donorId: item.donorId,
       donorName: item.donorName,
@@ -13,8 +15,7 @@ export async function GET(req, { params }) {
       engDate: item.engDate,
       storedBy: item.storedBy,
       remaining: item.remaining,
-      date:item.date
-      
+      date: item.date,
     }));
 
     return NextResponse.json(extractedData, { status: 200 });
