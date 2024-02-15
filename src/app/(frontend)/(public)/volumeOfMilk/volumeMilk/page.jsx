76f5d5845dee7fb0adbbaa90d9b70f9d87141c7c
sandip <sandeep47nepali@gmail.com>
2024-02-15 17:22:17 +0545

    changes

--- src/app/(frontend)/(public)/volumeOfMilk/volumeMilk/page.jsx
+++ src/app/(frontend)/(public)/volumeOfMilk/volumeMilk/page.jsx
@@ -128,7 +128,6 @@ export default function ListVolume() {
 
                 {filteredVolumeList?.map((item, index) => {
                   return (
-<<<<<<< HEAD
                     <>
                       {item ? (
                         <tr
@@ -199,50 +198,6 @@ export default function ListVolume() {
                         </tr>
                       )}
                     </>
-=======
-                    <tr
-                      className=" border border-x-gray text-center"
-                      key={index}
-                    >
-                      {/* <td className="py-3 text-center">
-                    <input type="checkbox" name="" id="" />
-                  </td> */}
-                      <td className="py-3">{index + 1}</td>
-                      <td className="py-3">{item.donorName}</td>
-                      {gestationalAgeList?.map((age, index) => {
-                        if (age.gestationalId === item.gestationalAge) {
-                          return (
-                            <td className="py-3" key={index}>
-                              {age.gestationalName}
-                            </td>
-                          );
-                        }
-                      })}
-                      {/* <td className="py-3">{item.donorId.contactNo}</td> */}
-                      <td className="py-3">{item.date}</td>
-                      <td className="py-3">{item.time}</td>
-                      <td className="py-3">{item.quantity} ml</td>
-                      <td className="py-3">
-                        <div className="flex justify-evenly text-xl">
-                          <div className=" cursor-pointer px-2 py-1 rounded-md shadow-md bg-lime-600">
-                            <PencilSquareIcon
-                              className="h-6 w-6 text-white"
-                              onClick={() => handleEdit(item._id)}
-                            />
-                          </div>
-                          <div className=" cursor-pointer px-2 py-1 rounded-md shadow-md bg-red-600">
-                            <TrashIcon
-                              className="h-6 w-6 text-white"
-                              onClick={() => handleDelete(item._id)}
-                            />
-                          </div>
-                          
-
-
-                        </div>
-                      </td>
-                    </tr>
->>>>>>> eabc3ef1395c785104aa15b4d5dd3fcb3f5cf5fe
                   );
                 })}
               </table>
