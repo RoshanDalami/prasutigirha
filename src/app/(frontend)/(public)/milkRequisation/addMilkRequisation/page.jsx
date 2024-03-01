989b2f51619a979a66a2e2b63acef0ca533f775b
aakash-22ak <akashshrestha678@gmail.com>
2024-03-01 10:37:47 +0545

    form field added

--- src/app/(frontend)/(public)/milkRequisation/addMilkRequisation/page.jsx
+++ src/app/(frontend)/(public)/milkRequisation/addMilkRequisation/page.jsx
@@ -5,7 +5,7 @@ import { NepaliDatePicker } from "nepali-datepicker-reactjs";
 import "nepali-datepicker-reactjs/dist/index.css";
 import BikramSambat, { ADToBS, BSToAD } from "bikram-sambat-js";
 import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { urls } from "src/services/apiHelpers";
@@ -26,9 +26,13 @@ export default function AddMilkReq({ clickedIdData }) {
     handleSubmit,
     formState: { isSubmitting, errors },
     setValue,
-    watch
+    control,
+    watch,
   } = useForm();
-
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "test",
+  });
   //gestationalAge
   const [gestationalAge, setGestationalAge] = useState([]);
   useEffect(() => {
@@ -49,24 +53,23 @@ export default function AddMilkReq({ clickedIdData }) {
     );
   });
 
-const [babyList,setBabyList] = useState([]);
-useEffect(()=>{
-  async function fetchData(){
-    const {status,data} = await axios.get(`${urls.getBaby}`);
-    if(status === 200){
-      setBabyList(data);
+  const [babyList, setBabyList] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const { status, data } = await axios.get(`${urls.getBaby}`);
+      if (status === 200) {
+        setBabyList(data);
+      }
     }
-  }
-  fetchData()
-},[])
-  
+    fetchData();
+  }, []);
 
-const watchFields = watch()
+  const watchFields = watch();
   const onSubmit = async (data) => {
     data = {
       ...data,
-      babyId: watchFields.babyId.split('/')[0]  ,
-      babyName: watchFields.babyId.split('/')[1]  ,
+      babyId: watchFields.babyId.split("/")[0],
+      babyName: watchFields.babyId.split("/")[1],
       _id: data?._id,
       userId: userInfo._id,
       feedingDate: feedingDate,
@@ -81,6 +84,10 @@ const watchFields = watch()
       console.log(error, "response");
     }
   };
+  function handleAppend(e) {
+    e.preventDefault();
+    append({});
+  }
 
   return (
     <>
@@ -90,27 +97,52 @@ const watchFields = watch()
         </div> */}
         <form onSubmit={handleSubmit((data) => onSubmit(data))}>
           <FormBorder title={"Milk Requisition Form"}>
-            
-            <p className="text-xl font-bold py-6">Feeding Details:</p>
+            <div className="flex justify-between mt-6">
+              <p className="text-xl font-bold ">Feeding Details:</p>
+              <div className="font-bold text-lg flex justify-end">
+                <button
+                  className="text-white bg-red-600 hover:bg-[#004a89] px-8 py-2 rounded-lg "
+                  onClick={handleAppend}
+                >
+                  Add More +
+                </button>
+              </div>
+            </div>
             <div className="grid md:grid-cols-2 grid-cols-1 text-lg gap-4">
               <div className="grid">
                 <label htmlFor="">
-                  Baby  <span className="text-red-600">*</span>
+                  Baby <span className="text-red-600">*</span>
                 </label>
-               <select name="" id="" className="inputStyle" {...register('babyId')} >
-                <option value={''} selected disabled >--Select Baby--</option>
-                {
-                  babyList?.map((item,index)=>{
-                    const combinedValue = `${item._id}/${item.babyName}`
-                    return(
+                <select
+                  name=""
+                  id=""
+                  className="inputStyle"
+                  {...register("babyId")}
+                >
+                  <option value={""} selected disabled>
+                    --Select Baby--
+                  </option>
+                  {babyList?.map((item, index) => {
+                    const combinedValue = `${item._id}/${item.babyName}`;
+                    return (
                       <option key={index} value={combinedValue}>
                         {item.babyName}
                       </option>
-                    )
-                  })
-                }
-               </select>
-               
+                    );
+                  })}
+                </select>
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="">
+                  Feeding Date<span className="text-red-600">*</span>
+                </label>
+                <NepaliDatePicker
+                  inputClassName="form-control  focus:outline-none"
+                  value={feedingDate}
+                  onChange={(e) => setFeedingDate(e)}
+                  options={{ calenderLocale: "ne", valueLocale: "en" }}
+                  className="inputStyle"
+                />
               </div>
               <div className="grid">
                 <label htmlFor="">
@@ -130,7 +162,8 @@ const watchFields = watch()
               </div>
               <div className="grid">
                 <label htmlFor="">
-                  Unique Bottle Number<span className="text-red-600">*</span>
+                  Unique Bottle Number
+                  <span className="text-red-600">*</span>
                 </label>
                 <input
                   type="text"
@@ -163,18 +196,7 @@ const watchFields = watch()
                   <p className="errorMessages">{errors.bottleName.message}</p>
                 )}
               </div>
-              <div className="flex flex-col">
-                <label htmlFor="">
-                  Feeding Date<span className="text-red-600">*</span>
-                </label>
-                <NepaliDatePicker
-                  inputClassName="form-control  focus:outline-none"
-                  value={feedingDate}
-                  onChange={(e) => setFeedingDate(e)}
-                  options={{ calenderLocale: "ne", valueLocale: "en" }}
-                  className="inputStyle"
-                />
-              </div>
+
               <div className="flex flex-col">
                 <label htmlFor="">
                   ML<span className="text-red-600">*</span>
@@ -192,6 +214,98 @@ const watchFields = watch()
                 )}
               </div>
             </div>
+            {fields.map((items, index) => {
+              return (
+                <div
+                  key={items.id}
+                  className="grid md:grid-cols-2 grid-cols-1 text-lg gap-4 pt-4"
+                >
+                  <div className="grid">
+                    <label htmlFor="">
+                      Batch Number <span className="text-red-600">*</span>
+                    </label>
+                    <input
+                      type="Number"
+                      className="inputStyle"
+                      placeholder="Enter Batch Number"
+                      {...register(`test.${index}.batchNumber`, {
+                        required: "Batch number is required",
+                      })}
+                    />
+                    {errors?.batchNumber && (
+                      <p className="errorMessages">
+                        {errors.batchNumber.message}
+                      </p>
+                    )}
+                  </div>
+                  <div className="grid">
+                    <label htmlFor="">
+                      Unique Bottle Number
+                      <span className="text-red-600">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      className="inputStyle"
+                      placeholder="Enter Unique Bottle Number"
+                      {...register(`test.${index}.uniqueBottleNumber`, {
+                        required: "Bottle number is required",
+                      })}
+                    />
+                    {errors?.uniqueBottleNumber && (
+                      <p className="errorMessages">
+                        {errors.uniqueBottleNumber.message}
+                      </p>
+                    )}
+                  </div>
+
+                  <div className="flex flex-col">
+                    <label htmlFor="">
+                      Bottle Name<span className="text-red-600">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      className="inputStyle"
+                      placeholder="Enter Bottle Name"
+                      {...register(`test.${index}.bottleName`, {
+                        required: "Bottle name is required",
+                      })}
+                    />
+                    {errors?.bottleName && (
+                      <p className="errorMessages">
+                        {errors.bottleName.message}
+                      </p>
+                    )}
+                  </div>
+
+                  <div className="flex flex-col">
+                    <label htmlFor="">
+                      ML<span className="text-red-600">*</span>
+                    </label>
+                    <input
+                      type="number"
+                      className="inputStyle"
+                      placeholder="Enter ML"
+                      {...register(`test.${index}.quantity`, {
+                        required: "Quantity is required",
+                      })}
+                    />
+                    {errors?.quantity && (
+                      <p className="errorMessages">{errors.quantity.message}</p>
+                    )}
+                  </div>
+                  <div></div>
+                  <div className="font-bold text-lg flex justify-end">
+                    <button
+                      className="text-white bg-red-600 hover:bg-[#004a89] px-8 py-2 rounded-lg "
+                      onClick={() => remove(index)}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+
             <div className="my-5 font-bold text-xl">
               <Button>{isSubmitting ? "Submiting ..." : "Submit"}</Button>
             </div>
