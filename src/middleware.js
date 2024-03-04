f9613245d351bcfffc545b690f56843c42b6e5ef
RoshanDalami <roshandalami0@gmail.com>
2024-03-04 20:26:00 +0545

    session

--- src/middleware.js
+++ src/middleware.js
@@ -13,9 +13,10 @@ export function middleware(req) {
   if (!token && !isPublicPath) {
     return NextResponse.redirect(new URL("/login", req.nextUrl));
   }
-  // if (decodedToken?.exp < currentTime && path != "/login") {
-  //   return NextResponse.redirect(new URL("/login", req.nextUrl));
-  // }
+
+  if (decodedToken?.exp < currentTime && path != "/login") {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
 }
 
 export const config = {
