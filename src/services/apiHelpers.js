ac76ffa86dc4b77c2eb8b7bcd5f3ec54a67b41cc
RoshanDalami <roshandalami0@gmail.com>
2025-06-08 08:24:35 +0545

    changed vps

--- src/services/apiHelpers.js
+++ src/services/apiHelpers.js
@@ -12,7 +12,7 @@ const token = Cookies.get("token");
 
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://www.prasuti.palikasoft.com"
+    ? "https://prasuti.palikasoft.com"
     : "http://localhost:8000";
 
 export const urls = {
